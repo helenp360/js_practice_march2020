76513863af5c99d9874fc9c5c26f8c005b2361c9
helenp360 <helloupart@gmail.com>
2020-04-19 21:19:02 +0100

    Completed camelCaseWords and getSquares

--- challenges/week3.js
+++ challenges/week3.js
@@ -1,11 +1,20 @@
 function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
-  // Your code here!
+  return nums.map(num => Math.pow(num, 2));
 }
-
+//["my", "variable"])).toBe("myVariable")
+//empty array
+//push words[0]
+//then i = 1 for loop
+//inside that loop capitalise first letter then return rest
 function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
-  // Your code here!
+  let newArr = [];
+  newArr.push(words[0]);
+  for (let i = 1; i < words.length; i++){
+    newArr.push(words[i][0].toUpperCase() + words[i].slice(1, words[i].length));
+  }
+  return newArr.join("");
 }
 
 function getTotalSubjects(people) {
