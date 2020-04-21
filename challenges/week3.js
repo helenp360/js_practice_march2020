620c9e90df54b205410995cc251fa6160701b258
helenp360 <helloupart@gmail.com>
2020-04-21 14:37:37 +0100

    Completed duplicateNumbers

--- challenges/week3.js
+++ challenges/week3.js
@@ -19,20 +19,66 @@ function camelCaseWords(words) {
 
 function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  let subjectArr = [];
+  for (let i = 0; i < people.length; i++){
+    if (people[i].subjects.length > 1){
+          subjectArr.push(people[i].subjects);
+    } 
+  }
+if (subjectArr.length < 1){
+  return 0;
+} else {
+  return subjectArr.toString().split(",").length;
 }
+}
+
+// function getTotalSubjects(people) {
+//   if (people === undefined) throw new Error("people is required");
+//   let subjectArr = [];
+//   for (let i = 0; i < people.length; i++){
+//     if (people[i].subjects.length > 1){
+//           subjectArr.push(people[i].subjects);
+//     } 
+//   }
+//   console.log(subjectArr);
+// if (subjectArr.length < 1){
+//   return 0;
+// } else {
+//   return subjectArr.toString().split(",").length;
+// }
+// }
+
+// function getSubjects (person) {
+//   return Object.values(person);
+// }
+
+// console.log(people.map(getSubjects));
 
 function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
-  // Your code here!
+  let valueArray = [];
+  for (let i = 0; i < menu.length; i++){
+    valueArray.push(Object.values(menu[i])); 
+  }
+  return valueArray.includes(ingredient);
 }
 
 function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
-  // Your code here!
+  let resultArr = [];
+  for (let i = 0; i < arr1.length; i++){
+    let num = arr1[i];
+    for (let i = 0; i < arr2.length; i++){
+      if (num === arr2[i] && resultArr.includes(num) === false){
+        resultArr.push(num);
+      }
+    }
+  }
+  return resultArr.sort();
 }
+
 
 module.exports = {
   getSquares,
