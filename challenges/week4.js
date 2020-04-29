16c4e4f65a190fa24d202635af3795b4fa1752c6
helenp360 <helloupart@gmail.com>
2020-04-29 20:17:10 +0100

    Completed first 4 functions

--- challenges/week4.js
+++ challenges/week4.js
@@ -1,22 +1,46 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  let newArr = [];
+  for (let i = 0; i < nums.length; i++){
+    if (nums[i] < 1){
+      newArr.push(nums[i]);
+    }
+  }
+  return newArr;
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  // Your code here
+  let newArr = [];
+  for (let i = 0; i < names.length; i++){
+    if (names[i][0] === char){
+      newArr.push(names[i])
+    }
+  }
+  return newArr;
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  // Your code here
+  let newArr = [];
+  for (let i = 0; i < words.length; i++){
+    if (words[i].includes("to ")){
+      newArr.push(words[i]);
+    }
+  }
+  return newArr;
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  let newArr = [];
+  for (let i = 0; i < nums.length; i++){
+    if (Number.isInteger(nums[i]) === true){
+      newArr.push(nums[i]);
+    }
+  }
+  return newArr;
 }
 
 function getCities(users) {
@@ -37,7 +61,7 @@ function findSentencesContaining(sentences, str) {
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-  // Your code here
+  // sort then return last one?
 }
 
 module.exports = {
