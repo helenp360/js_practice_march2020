a0a1f54e593d3f5ecc3127555eb56d90374182eb
helenp360 <helloupart@gmail.com>
2020-05-23 21:36:19 +0100

    Completed findNextNumber and reverseNumber

--- challenges/week8.js
+++ challenges/week8.js
@@ -1,8 +1,18 @@
 const findNextNumber = (nums, n) => {
   if (nums === undefined) throw new Error("nums is required");
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+  if (nums.includes(n) === false){
+    return null;
+  } else if (nums.includes(n) === true){
+    if (nums.indexOf(n) === nums.length - 1){
+      return null;
+    } else {
+      let myIndex = nums.indexOf(n);
+      return nums[myIndex + 1]
+    }
+  }
 };
+
 
 const count1sand0s = str => {
   if (str === undefined) throw new Error("str is required");
@@ -11,7 +21,12 @@ const count1sand0s = str => {
 
 const reverseNumber = n => {
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+  let arr = n.toString().split("");
+  let backwardsArr = [];
+  for (let i = arr.length - 1; i >= 0; i--){
+    backwardsArr.push(arr[i])
+  }
+  return parseInt(backwardsArr.join().replace(/,/g, ""));
 };
 
 const sumArrays = arrs => {
